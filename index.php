35700c3006e6dea6cb18983950841fc9a6ff20ac
mzrpour <125277638+babayiu@users.noreply.github.com>
2023-10-11 05:01:11 +0200

    Update index.php

--- index.php
+++ index.php
@@ -48,8 +48,6 @@ file_get_contents($url . '?' . http_build_query($data));
 	function curl($url,$timeout=7){		
 		$ch = curl_init();
 		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		//curl_setopt($ch, CURLOPT_SSLVERSION,3);
 		curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
 		//curl_setopt($ch, CURLOPT_USERAGENT, $_REQUEST['HTTP_USER_AGENT']);
 		curl_setopt ($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
