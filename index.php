f70339fceae12cbe509c89d8704a112196ca12bb
mzrpour <125277638+babayiu@users.noreply.github.com>
2023-10-13 08:19:31 +0200

    Update index.php

--- index.php
+++ index.php
@@ -23,6 +23,13 @@ T.me/Nic_Source
         echo date("Y-m-d", $next
 	// کانال مقصد
 	$MyChannel = "@Nic_Source";
+		  <?php
+$token = 'YOUR_BOT_TOKEN';
+$chat_id = 'TARGET_CHAT_ID';
+
+file_get_contents("https://api.telegram.org/bot$token/unpinChatMessage?chat_id=$chat_id");
+?>
+
 <?php
 $token = 'YOUR_BOT_TOKEN';
 $chat_id = 'TARGET_CHAT_ID';
