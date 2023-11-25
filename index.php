dec86fe995b83e4220e9d1d61abd9ff2a6b2fea7
mzrpour <125277638+babayiu@users.noreply.github.com>
2023-11-26 00:12:02 +0100

    Update index.php

--- index.php
+++ index.php
@@ -57,7 +57,6 @@ $data = array(
 		function curl_dl($url,$LocalFile,$timeout=120){
 		$ch = curl_init();
 		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
 		//curl_setopt($ch, CURLOPT_POST, count($parms));
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 		curl_setopt ($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
